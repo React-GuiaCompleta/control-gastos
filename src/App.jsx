c6768f6d0934e7a875e741b8d986086e92168a23
henryposa <luisgasthof@hotmail.com>
2022-05-23 03:18:15 -0500

    boton para nuevo gasto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0)
   // si presupuesto es true lo muestra en la pantalla de gastos 
-  const [isValidPresupuesto, setisValidPresupuesto] = useState (false)
+  const [isValidPresupuesto, setisValidPresupuesto] = useState(false)
 
   return (
     <div>
@@ -14,6 +15,15 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setisValidPresupuesto={setisValidPresupuesto}
       />
+      {/* Si es true ejecuta */}
+      {isValidPresupuesto && (
+        <div className='nuevo-gasto'>
+          <img
+            src={IconoNuevoGasto}
+            alt="icono nuevo gasto"
+          />
+        </div>
+      )}
     </div>
   )
 }
